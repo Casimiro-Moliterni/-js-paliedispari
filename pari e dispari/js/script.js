49cd6c50e631dbb2a151372821f222444db0241b
Casimiro Moliterni <moliternicasimiro@hotmail.it>
2024-03-14 15:14:32 +0100

    add : question user number and even or odd

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -8,3 +8,20 @@
 // Consigli del giorno
 // 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
 // 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+
+
+// utente sceglie pari o dispari con un prompt 
+
+// utente scrive un numero da  1 a 5 con un prompt 
+
+// creare funzione che genera un numero random da 1 a 5 
+
+// poi sommare il numero random con il numero scleto dall'utente 
+
+//   creare funzione che stabilisce se il risulto della somma è pari o dispari si decreta il vincitore in base alla scelta dell'utente di pari o dispari 
+
+const userEvenorOdd = prompt('Pari o Dispari ? scegli');
+console.log(userEvenorOdd)
+
+const userNumber = parseInt(prompt('Scegli un numero da 1 a 5 !'));
+console.log(userNumber)
