83d3cdf32d9a697cf2cf8dcc06992b119b1ecc7a
Casimiro Moliterni <moliternicasimiro@hotmail.it>
2024-03-14 16:42:57 +0100

    add : function for revert word,  add : function to determine whether a word is a palindrome

--- parole palindrome/js/script.js
+++ parole palindrome/js/script.js
@@ -16,10 +16,34 @@
 // dal primo verso che dal secondo 
 
 
-const userWord = prompt('Inserisci una parola');
-console.log(userWord)
+let userWord = prompt('Inserisci una parola');
 
-function wordPalindromCalculator(word){
+const wordResult = wordRevert(userWord);
+console.log(wordResult)
 
+
+
+function wordRevert(word){
+
+ 
+    // for ( let i = 0 ; i < userWord.length ; i++){
+    //     console.log(userWord[i])};
     
+    let revertWord = '';
+    for ( let i = userWord.length - 1 ; i >= 0 ; i--){
+        const thisRevertWord = userWord[i];
+
+        revertWord += thisRevertWord;
+    };
+}
+
+function calculatorWordPalindrom(reverword){
+    let message ;
+    if ( revertWord === userWord){
+          message = 'la parola è palindroma'
+
+    }else{
+        message = 'la parola non è palindroma'
+    }
+    return message;
 }
